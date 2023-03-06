ef068cca68eba57d0d45821b81c31cba14769d7c
kangzuyao <351658471@qq.com>
2023-03-06 17:32:52 +0800

    fix：评论接口修改

--- router_handle/comments_handle.js
+++ router_handle/comments_handle.js
@@ -1,20 +1,29 @@
 const db = require('../db/index')
 
 exports.addComment = (req, res)=>{
+  const from_id = req.auth.userId
   const createTime = new Date().getTime().toString()
   const id = (Math.random()+ new Date().getTime()).toString(32).slice(0,8)
-  const data = {...req.body, id, createTime}
+  const status = 0
+  const praise = 0
+  const data = {...req.body, id, createTime,from_id, status, praise}
+  console.log('data',data);
   const sqlStr = "INSERT INTO comments SET ?"
   db.query(sqlStr, data, (err,  result)=> {
-    if(err)return res.send({code:-1,msg:'评论失败'})
+    if(err){
+      console.log('err',err);
+      return res.send({code:-1,msg:'评论失败'})
+    }
     res.send({
       code:0,
-      msg:'评论成功'
+      msg:'评论成功',
+      id
     })
   })
 }
 
 exports.queryComment = (req, res)=> {
+  const users = {}
   const data = req.query
   const {page, pageSize} = data
   const start = (page - 1)* pageSize
@@ -35,6 +44,31 @@ exports.queryComment = (req, res)=> {
   const tempArr = JSON.stringify(data) === '{}'? [start, pageSize*1]: [...tempValues, start, pageSize*1]
   db.query(sqlStr, tempArr, (err, result) => {
     if(err) return res.send({code:-1,msg:err})
+    result.forEach(item => {
+      if(users[item.from_id]){
+        item.from_nickName = users[item.from_id]
+      }else{
+        const userSqlStr = `SELECT nickName FROM users WHERE userId = ?`
+        db.query(userSqlStr, item.from_id, (err, userData)=> {
+          if(err)return res.send({code:-1,msg:err})
+          users[item.from_id] = userData[0].nickName
+          item.from_nickName = userData[0].nickName
+        })
+      }
+      if(item.to_uid){
+        if(users[item.to_uid]){
+          item.to_nickName = users[item.to_uid]
+        }else{
+          const userSqlStr = `SELECT nickName FROM users WHERE userId = ?`
+          db.query(userSqlStr, item.to_uid, (err, userData)=> {
+            if(err)return res.send({code:-1,msg:err})
+            users[item.to_uid] = userData[0].nickName
+            item.to_nickName = userData[0].nickName
+          })
+        }
+      }
+      
+    })
     const sqlCountStr = `SELECT  count(*) as total FROM comments ${where}`
     db.query(sqlCountStr,tempArr, (err_c, result_c) => {
       if(err_c)return res.send({})
